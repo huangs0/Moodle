77ec732110c6bf7e9ac205dc0f799ad6e521c75a
huangs0 <59648015+huangs0@users.noreply.github.com>
2021-01-08 22:02:23 +0800

    Update section.js

--- node-server/section.js
+++ node-server/section.js
@@ -1,103 +1,107 @@
-let express = require('express');
+/**
+ * This project is written in ES6
+ * 
+ * @author Huang Songlin
+ */
+import express from 'express';
 let router = express.Router();
 
-/**
- * Protocol for request under this router
- * 1. '/main/:course_id' POST: couse_id should provide in url
- * POST body should have title, content as compulsory part
- * Server->Client: Text Message, 'Fail' if server fail to process your request or section_id if success
- * 2. '/sub/:section_id' POST: section_id should provide in url
- * POST body should have title, content, type as compulsory part
- * Server->Client: Text Message, 'Fail' if server fail to process your request or subsection_id if success
- */
-
 // Add new section to a course
-router.post('/main/:course_id',(req,res)=>{
+router.post('/section/:course_id',(req,res)=>{
     if (req.identity != 'instructor'){
-        res.end('Fail');
+        res.json({
+            status:false,
+            error: 'You are not the instructor'
+        });
     }
-    req.sql.query('SELECT `instructor` FROM `course` WHERE `course_id` = '+req.params.course_id+';',(err,results,fields)=>{
+    req.sql.query(`SELECT instructor FROM course WHERE course_id = '${req.params.course_id}';`,(err,results,fields)=>{
         let instructor = JSON.parse(results[0].instructor);
         if (instructor.indexOf(req.cookies.uid) == -1){
-            res.end('Fail');
+            res.json({
+                status:false,
+                error: 'You are not the instructor of this course'
+            });
         }
     })
-    req.sql.query('INSERT INTO `section` (`course_id`,`title`,`content`,`type`) VALUES (`'+req.params.course_id,
-    +'`,`'+req.body.title+'`,`'+req.body.content+'`,`lecture`);',(err,results,fields)=>{
+    req.sql.query(`INSERT INTO section (course_id,title,content,type) VALUES ('${req.params.course_id}','${req.body.title}','${req.body.content}','lecture');`
+    ,(err,results,fields)=>{
         if (err){
             console.log(err.messages);
-            res.end('Fail');
+            res.json({
+                status:false,
+                error: 'Fail to post to database'
+            });
         }
     })
-    req.sql.query('SELECT `section_id` FROM `section` WHERE `title` = '+req.body.title+' AND `course_id` = '
-    +req.params.course_id+';',(err, results, fields)=>{
+    req.sql.query(`SELECT section_id FROM section WHERE title = '${req.body.title}' AND course_id = '${req.params.course_id}';'`
+    ,(err, results, fields)=>{
         if (err){
             console.log(err.message);
         }
         else {
             // send back section_id for register source
-            res.send(results[0].section_id);
+            res.json({
+                status: true,
+                section_id: results[0].section_id
+            });
         }
     })
 })
 
 // Add a subsection to a main section, i.e. Tutorial or Workshop or Readings to the Lecture
-router.post('/sub/:section_id',(req,res)=>{
+router.post('/subsection/:section_id',(req,res)=>{
     if (req.identity != 'instructor'){
-        res.end('Fail');
+        res.json({
+            status:false,
+            error: 'You are not the instructor'
+        });
     }
-    req.sql.query('SELECT `instructor` FROM `course` WHERE `course_id` = '+req.params.course_id+';',(err,results,fields)=>{
+    req.sql.query(`SELECT instructor FROM course WHERE course_id = '${req.params.course_id}';`,(err,results,fields)=>{
         let instructor = JSON.parse(results[0].instructor);
         if (instructor.indexOf(req.cookies.uid) == -1){
-            res.end('Fail');
+            res.json({
+                status:false,
+                error: 'You are not the instructor of this class'
+            });
         }
     })
-    req.sql.query('SELECT `course_id` FROM `section` WHERE `section_id` = '+req.params.section_id+';',(err,results,fields)=>{
+    req.sql.query(`SELECT course_id FROM section WHERE section_id = '${req.params.section_id}';`,(err,results,fields)=>{
         if (err){
             console.log(err.message);
-            res.end('Fail');
+            res.json({
+                status: false,
+                error: 'Fail to select from database'
+            });
         }
         else {
             req.course_id = results[0].course_id;
         }
     })
     // Add the subsection to the subsection database
-    req.sql.query('INSERT INTO `subsection` (`title`,`type`,`content`,`section_id`,`course_id`) VALUES (`'+
-    req.body.title+'`,`'+req.body.type+'`,`'+req.body.content+'`,`'+req.params.section_id+'`,`'+req.course_id+'`);',(err,results,fields)=>{
+    req.sql.query(`INSERT INTO subsection (title,type,content,section_id,course_id) VALUES ('${req.body.title}','${req.body.type}','${req.body.content}','${req.params.section_id}','${req.course_id}');`
+        ,(err,results,fields)=>{
         if (err){
             console.log(err.message);
-            res.end('Fail');
+            res.json({
+                status:false,
+                error: 'Fail to add to database'
+            });
         }
     })
-    req.sql.query('SELECT `subsection_id` FROM `subsection` WEHRE `title` = `'+req.body.title+'` AND `type` = `'+req.body.type+'` AND `section_id` = `'+req.params.section_id+'`;', (err,results,fields)=>{
+    req.sql.query(`SELECT subsection_id FROM subsection WEHRE title = '${req.body.title}' AND type = '${req.body.type}' AND section_id = '${req.params.section_id}';`, (err,results,fields)=>{
         if (err){
             console.log(err.message);
-            res.end('Fail');
+            res.json({
+                status:false,
+                error: 'Fail to select from database'
+            });
         }
         else {
             // Send back the subsection_id for register source
-            res.send(results[0].subsection_id);
-        }
-    })
-})
-
-// THE FOLLOWING PART IS IN AS-IS STATUS
-// Get a section?
-router.get('/:course_id',(req,res)=>{
-    req.sql.query('SELECT `title`, `section_id` FROM `section` WHERE `course_id` = '+req.params.course_id+';',(err,results,fields)=>{
-        if (err){
-            console.log(err.message);
-            res.send('Fail');
-        }
-        else {
-            let return_json = [];
-            for (let i = 0; i < results.length; i++){
-                return_json.push({
-                    title: results[i].title,
-                    section_id: results[i].section_id
-                })
-            }
-            res.json(return_json);
+            res.json({
+                status: true,
+                subsection_id: results[0].subsection_id}
+            );
         }
     })
 })
